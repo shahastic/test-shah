93c97d79ff97313c905fce1e5f67dead4a9674cd
shahastic <shahr6485@gmail.com>
2022-01-23 04:12:34 +0530

    sixth commit

--- index.php
+++ index.php
@@ -1,26 +1,20 @@
 
 <?php
+session_start();
+include '_dbConnect.php';
+require("vendor/autoload.php");
+require_once("mailer/PHPMailer.php");
+require_once("mailer/SMTP.php");
+require_once("mailer/Exception.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'vendor/autoload.php';
+?>
+
+<?php
 
 
-
-
-
-// //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-
-
-
-$showAlert = false;
-$showError = false;
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
