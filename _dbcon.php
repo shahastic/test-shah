d0ac043c478dcf6d4a21cc2965cdb0769fbc844d
shahastic <shahr6485@gmail.com>
2022-01-23 03:50:15 +0530

    third commit

--- _dbcon.php
+++ _dbcon.php
@@ -1,10 +1,17 @@
 <?php 
 
-//connecting to db
+//connecting to herokudb
 $servername = "7faf2220@us-cdbr-east-05.cleardb.net";
 $email = "bcffd359fc0e1a";
 $password = "7faf2220";
 $database = "heroku_27ad2dd43bf0955";
+
+
+//connecting to db
+$servername = "localhost";
+$email = "email";
+$password = "";
+$database = "test";
 
 //create a connection
 $conn = mysqli_connect($servername , $email , $password  , $database );
